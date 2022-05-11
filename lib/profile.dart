1829361a6a5500237cfc93f277138225c457bdee
Valentin Schaefer <schaefer.valentin64@gmail.com>
2022-05-11 14:44:21 +0200

    new update with graphicals changements

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -11,8 +13,8 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        title: Text("My Profil", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.black)),
+        backgroundColor: Color.fromARGB(255, 105, 94, 245),
+        title: Text("My Profil", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white)),
         // iconTheme: Icon(Icons.card_travel),
         centerTitle: true,
         elevation: 0.0,
@@ -26,32 +28,34 @@ class _ProfileState extends State<Profile> {
               children: <Widget>[
                 Image (
                     image: AssetImage('assets/images/profile.png'),
-                    width: 100.0,
-                    height: 100.0,
+                    width: 80.0,
+                    height: 80.0,
                     alignment: Alignment.topCenter,
                 ),
                 SizedBox(height: 10.0
                 ),
-                Text("Valentin Schaefer", textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Rubik', fontSize: 25, color: Colors.black)
+                Text("Valentin Schaefer", textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.black)
                 ),
               ],
             ),
           ),
-          SizedBox(height: 15.0
+          SizedBox(height: 10.0
           ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                  Text("Last Parkour", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 20, color: Colors.black)),
-                  SizedBox(height: 10.0
+                  Text("Last Parkour", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.black)),
+                  SizedBox(height: 5.0
                   ),
                   // ListView
                   Container(
                     // padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 150.0),
                     alignment: Alignment.centerLeft,
-                    child: Column(
+                    height: 300,
+                    width: 500,
+                    child: Stack(
                         children: <Widget>[
                           ClipRRect(
                             borderRadius: BorderRadius.circular(30.0),
@@ -59,6 +63,31 @@ class _ProfileState extends State<Profile> {
                                     image: AssetImage('assets/images/effeil_tower.jpg'),
                                     height: 325.0,
                             ),
+                          ),
+                          Container(
+                              padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                              child: Text("Eiffel Tower", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.white,
+                                  shadows: <Shadow>[
+                                    Shadow(
+                                      offset: Offset(1.0, 1.0),
+                                      blurRadius: 10.0,
+                                      color: Color.fromARGB(255, 0, 0, 0),
+                                    ),
+                                  ]
+                              )),
+                          ),
+                          Container(
+                            width: 700,
+                            padding: EdgeInsets.symmetric(horizontal: 145.0, vertical: 10.0),
+                            child: Text("4,3*", textAlign: TextAlign.center, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.yellow,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(0.5, 0.5),
+                                    blurRadius: 10.0,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ]
+                            )),
                           ),
                         ],
                     )
@@ -69,15 +98,16 @@ class _ProfileState extends State<Profile> {
           SizedBox(height: 15.0
           ),
           Container(
+            width: 500,
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
-                Text("Last Comment", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 20, color: Colors.black)
+                Text("Last Comment", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.black)
                 ),
                 SizedBox(height: 10.0
                 ),
-                Text("My last visit at Bordeaux was great but I don’t really recommand the tram. There is a lot of people.", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 15, color: Colors.black)
+                Text("My last visit at Bordeaux was great but I don’t really recommand the tram. There is a lot of people.", textAlign: TextAlign.left, style: TextStyle(fontFamily: 'Rubik', fontSize: 12, color: Colors.black)
                 ),
                 ],
             ),
